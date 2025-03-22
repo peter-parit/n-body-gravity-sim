44b13e10eeee9a8091d7e1f59291bbdbc03d42df
peterparit <paritv2003@gmail.com>
2025-03-22 19:57:19 +0700

    Update QuadTreeTest.scala

--- src/test/scala/QuadTreeTest.scala
+++ src/test/scala/QuadTreeTest.scala
@@ -2,14 +2,14 @@ object QuadTreeTest {
     def main(args: Array[String]): Unit = {
         val b: Boundary = new Boundary(new Point(0.0, 0.0), new Point(2.0, 2.0))
         val tree: QuadTree = new QuadTree(b)
-        if(tree.insert(new Point(0.5, 0.5))) then println("Added Point(0.5, 0.5)") // #
-        if(tree.insert(new Point(0.5, 1.5))) then println("Added Point(0.5, 1.5)") // .
-        if(tree.insert(new Point(0.4, 0.2))) then println("Added Point(0.4, 0.2)") // *
-        println((tree.topLeftTree.point.get.x, tree.topLeftTree.point.get.y))
-        println((tree.bottomLeftTree.bottomLeftTree.point.get.x,
-                 tree.bottomLeftTree.bottomLeftTree.point.get.y))
+        if(tree.insert(new ParBody(0.5, 0.5, 10, 10))) then println("Added Point(0.5, 0.5)") // .
+        if(tree.insert(new ParBody(0.5, 1.5, 10, 10))) then println("Added Point(0.5, 1.5)") // #
+        if(tree.insert(new ParBody(0.4, 1.8, 10, 10))) then println("Added Point(0.4, 0.2)") // *
+        println((tree.topLeftTree.body.get.x, tree.topLeftTree.body.get.y))
+        println((tree.bottomLeftTree.bottomLeftTree.body.get.x,
+                 tree.bottomLeftTree.bottomLeftTree.body.get.y))
 
-        /* Rough Sketch of the tree above: 
+        /* Rough Sketch of the tree above: (scalafx's positioning has (0,0) at top left.)
         ________________
         |       |       |
         |   .   |       |
