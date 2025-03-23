24596f6566748e13a47f927a4746557f73d7f9aa
peterparit <paritv2003@gmail.com>
2025-03-23 22:28:57 +0700

    Tried the circle simulation with naive

--- src/main/scala/naive-simulation/NaiveSim.scala
+++ src/main/scala/naive-simulation/NaiveSim.scala
@@ -11,6 +11,7 @@ import scalafx.scene.layout.Pane
 import scalafx.scene.input.MouseEvent
 import scalafx.Includes.*
 import scalafx.stage.Screen
+import scala.util.Random
 
 object NaiveSim extends JFXApp3 {
 
@@ -37,14 +38,24 @@ object NaiveSim extends JFXApp3 {
 
         // create n bodies to the screen
         val NUM_BODIES = 2000
+        val BODY_MASS = 10e11
+        val RADIUS = 1
+        val EPSILON = 10
+        val G = 6.67e-11
+        val random = new Random(123) // set seed for reproducibility (potentially when evaluating the run-time)
+        val circleRadius = screenHeight / 2
 
         var bodies: ListBuffer[NaiveBody] = ListBuffer()
         for (_ <- 0 until NUM_BODIES) {
+          val angle = random.nextDouble() * 2 * Math.PI
+          val r = random.nextDouble() * circleRadius
+
           val newNaiveBody = new NaiveBody(
-            Math.random() * screenWidth,
-            Math.random() * screenHeight,
-            10e11,
-            2
+            (screenWidth / 2) + r * Math.cos(angle),
+            (screenHeight / 2) + r * Math.sin(angle), 
+            BODY_MASS,
+            RADIUS,
+            G
           )
           main.children.add(newNaiveBody.naiveBody)
           bodies.append(newNaiveBody)
@@ -59,7 +70,7 @@ object NaiveSim extends JFXApp3 {
             val elapsed = (t - lastTime) / 1e9
 
             bodies.foreach { node =>
-              node.update(elapsed, bodies)
+              node.update(elapsed, bodies, EPSILON)
             }
           }
           lastTime = t
