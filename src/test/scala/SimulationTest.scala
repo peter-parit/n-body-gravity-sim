3ec3185a4762ebe8f430ef6ba756463fc44e90de
peterparit <paritv2003@gmail.com>
2025-03-31 18:24:44 +0700

    Update SimulationTest.scala

--- src/test/scala/SimulationTest.scala
+++ src/test/scala/SimulationTest.scala
@@ -5,15 +5,20 @@ import OptimizedSimTest.*
 object SimulationTest {
     def main(args: Array[String]): Unit = {
 
-        val N = 1000
+        val N = 100000
         
         print(f"Simulation Performance Test on $N bodies with 10 iterations\n\nAverage time [Naive]: ")
         val avgNaive = NaiveSimTest.run(N)
         println(f"$avgNaive%1.3f seconds")
 
-        print("\nAverage time [Optimized]: ")
-        val avgOpt = OptimizedSimTest.run(N)
+        print("\nAverage time [Barnes-Hut]: ")
+        val avgOpt = OptimizedSimWithoutParTest.run(N)
         println(f"$avgOpt%1.3f seconds")
+
+        print("\nAverage time [Barnes-Hut + Par]: ")
+        val avgOptPar = OptimizedSimTest.run(N)
+        println(f"$avgOptPar%1.3f seconds")
+
 
         println("\nAll done!")
 
